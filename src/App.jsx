01b3a6e5427ae60bdc88f4a4472fcca2621948d6
Samuel-for-github <134806257+Samuel-for-github@users.noreply.github.com>
2024-05-23 16:34:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+function App() {
+  const [facts, setFacts] = useState([])
+  useEffect(()=>{
+    axios.get('/api/facts')
+    .then((response)=>{
+      setFacts(response.data);
+    })
+  })
+  return (
+    <>
+     <h1>Hello</h1>
+
+    <p>facts: {facts.length}</p>
+    {
+      facts.map((fact)=>(
+        <div key={fact.id}>
+          <h3>{fact.title}</h3>
+          <h4>{fact.content}</h4>
+        </div>
+      ))
+    }
+    </>
+  )
+}
+
+export default App
